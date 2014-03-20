920039c10b98cd87fa567b821f03b5b9608ffd25
Jonathan Baker <jonathan@younger-associates.com>
2014-03-20 14:27:31 -0500

    Initial commit

---
+++ assets/js/libs/search/app.js
@@ -0,0 +1,9 @@
+// @codekit-append "controllers/TestController.js"
+
+(function() {
+    'use strict';
+
+    angular.module('app', [ 'app.controllers' ]);
+
+    angular.module('app.controllers', []);
+})();
---
+++ assets/js/min/app-ck.js
@@ -0,0 +1,1 @@
+!function(){"use strict";angular.module("app",["app.controllers"]),angular.module("app.controllers",[])}(),function(){"use strict";angular.module("app.controllers").controller("TestController",["$scope",function(o){o.test="TestController Text"}])}();
