d5b9924d8aabd685559c46306a194b5a15f0e0b9
Matt Snyder <matt@obscurelogic.com>
2014-05-30 07:49:02 -0500

    Typeahead cleanup.

--- public/assets/js/min/app-ck.js
+++ public/assets/js/min/app-ck.js
@@ -36331,26 +36331,14 @@ $(window).load(function(){
 (function () {
     "use strict";
 
-    var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-        'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-        'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-        'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-        'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-        'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-        'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-        'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-        'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-    ];
-
 // constructs the suggestion engine
     var clients = new Bloodhound({
         datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
-        // `states` is an array of state names defined in "The Basics"
         prefetch: '/typeahead/clients'
     });
 
-// kicks off the loading/processing of `local` and `prefetch`
+    // kicks off the loading/processing of `local` and `prefetch`
     clients.initialize();
 
     $('.typeahead').typeahead({
@@ -36361,13 +36349,12 @@ $(window).load(function(){
         {
             name: 'clients',
             displayKey: 'name',
-            // `ttAdapter` wraps the suggestion engine in an adapter that
-            // is compatible with the typeahead jQuery plugin
             source: clients.ttAdapter(),
             templates: {
                 header: '<h3>Clients</h3>',
 //                suggestion: Handlebars.compile('<p><strong>{{name}}</strong></p>')
             }
+
         });
 
     // Uncloak password on hover.
