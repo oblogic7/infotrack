175d23b1fe89a47cf2ee79165935b6089fe763c5
Matt Snyder <matt@younger-associates.com>
2014-08-25 16:21:44 -0500

    clean up

--- vendor/adamwathan/eloquent-oauth/src/AdamWathan/EloquentOAuth/OAuthIdentity.php
+++
@@ -1,8 +0,0 @@
-<?php namespace AdamWathan\EloquentOAuth;
-
-use Illuminate\Database\Eloquent\Model as Eloquent;
-
-class OAuthIdentity extends Eloquent
-{
-	protected $table = 'oauth_identities';
-}
